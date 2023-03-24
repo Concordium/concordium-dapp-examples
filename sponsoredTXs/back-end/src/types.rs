363a3769f179a23d3343fc23f5d16f685cfaf806
Doris Benda <DOBEN@users.noreply.github.com>
2023-03-24 20:50:38 +0200

    Fix signature

--- sponsoredTXs/back-end/src/types.rs
+++ sponsoredTXs/back-end/src/types.rs
@@ -1,9 +1,12 @@
 use concordium_rust_sdk::{
     endpoints::{QueryError, RPCError},
     smart_contracts::common::{
-        AccountAddress, Address, ContractAddress, OwnedEntrypointName, Serialize, Timestamp,
+        AccountAddress, ContractAddress, OwnedEntrypointName, Serialize, Timestamp, Serial,
     },
 };
+
+use concordium_rust_sdk::cis2::UpdateOperator;
+use concordium_rust_sdk::cis2::Transfer;
 
 use std::collections::BTreeMap;
 
@@ -66,59 +69,19 @@ pub struct TransferInputParams {
 #[repr(transparent)]
 pub struct SignatureEd25519(pub [u8; 64]);
 
-#[derive(Debug, Serialize, Clone)]
-pub struct UpdateOperator {
-    pub update: OperatorUpdate,
-    pub operator: Address,
-}
-
-#[derive(Debug, Serialize, Clone, Copy)]
-pub enum OperatorUpdate {
-    Remove,
-    Add,
-}
-
-pub type ContractTokenId = u32;
-//TokenIdU32;
-//u32;
-
-pub type ContractTokenAmount = u8;
-//TokenAmountU8;
-//u8;
-
-pub type TransferParameter = TransferParams;
-
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serial, Clone)]
 pub struct TransferParams(#[concordium(size_length = 2)] pub Vec<Transfer>);
 
-#[derive(Debug, Serialize, Clone)]
-pub struct Transfer {
-    pub token_id: ContractTokenId,
-    pub amount: ContractTokenAmount,
-    pub from: Address,
-    pub to: Receiver,
-    pub data: AdditionalData,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub enum Receiver {
-    Account(AccountAddress),
-    Contract(ContractAddress, OwnedEntrypointName),
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub struct AdditionalData(#[concordium(size_length = 2)] pub Vec<u8>);
-
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serial, Clone)]
 pub enum PermitPayload {
-    Transfer(TransferParameter),
+    Transfer(TransferParams),
     UpdateOperator(UpdateOperatorParams),
 }
 
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serial, Clone)]
 pub struct UpdateOperatorParams(#[concordium(size_length = 2)] pub Vec<UpdateOperator>);
 
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serial, Clone)]
 pub struct PermitParam {
     #[concordium(size_length = 1)]
     pub signature: BTreeMap<u8, BTreeMap<u8, SignatureEd25519>>,
@@ -126,7 +89,7 @@ pub struct PermitParam {
     pub message: PermitMessage,
 }
 
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serial, Clone)]
 pub struct PermitMessage {
     pub contract_address: ContractAddress,
     pub entry_point: OwnedEntrypointName,
