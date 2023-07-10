b3a6e64bb27f11eda61bc35215626235c60fcedc
Doris Benda <DOBEN@users.noreply.github.com>
2023-07-10 16:14:43 +0300

    Add link to source code

---
+++ signMessage/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.2.0
+
+-   Add link to source code
+
+## 0.1.0
+
+-   Initial signing frontend
---
+++ signMessage/src/Footer.tsx
@@ -0,0 +1,22 @@
+import { Col } from 'react-bootstrap';
+import packageInfo from '../package.json';
+
+function Footer() {
+    return (
+        <>
+            <hr />
+            <Col style={{ textAlign: 'center' }}>
+                Version: {packageInfo.version} |{' '}
+                <a
+                    href="https://github.com/Concordium/concordium-dapp-examples/tree/main/signMessage"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Source code.
+                </a>
+            </Col>
+        </>
+    );
+}
+
+export default Footer;
