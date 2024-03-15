f506a482a540c341053dd85d08b8bcb8a74c88fc
Lasse Møldrup <lasse.moeldrup@gmail.com>
2024-03-15 14:27:30 +0100

    ESLint and Prettier for signMessage

--- signMessage/src/index.tsx
+++ signMessage/src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
