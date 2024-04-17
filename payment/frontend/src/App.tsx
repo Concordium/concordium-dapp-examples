bffcc64a8387bca7fb0429abfcefa26ee3b20f16
Emil Holm Gjørup <eg@concordium.com>
2024-04-17 16:04:21 +0200

    Add QR code to UI

--- payment/frontend/src/App.tsx
+++ payment/frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Button from "react-bootstrap/Button";
 import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQrcode } from "@fortawesome/free-solid-svg-icons"
+import { QRCode } from 'react-qrcode-logo';
+import { QrScanner } from '@yudiel/react-qr-scanner';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
 
 import './styles.scss';
+import { ContractAddress } from '@concordium/web-sdk';
 
 export const App = () => {
     return (
@@ -20,33 +26,105 @@ export const App = () => {
 };
 
 function SendForm() {
-    return (
-          <Container className="row align-items-center justify-content-center">
-                <Row>
-                  <Col>
-                    <Form>
-                      <Form.Label htmlFor="basic-url">Send</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Text id="amount">EUR</InputGroup.Text>
-                        <Form.Control
-                            placeholder="3.14"
-                            aria-label="EUR amount"
-                            aria-describedby="amount-balance"
-                        />
-                      </InputGroup>
-                      <Form.Text id="amount-balance" muted>
-                        Current balance is EUR 100,000.00
-                      </Form.Text>
-                      <Form.Group className="mb-3" controlId="formReceiver">
-                        <Form.Label>To</Form.Label>
-                        <Form.Control type="text" placeholder="" />
-                      </Form.Group>
-                      <Button>Send</Button>
-                    </Form >
-                  </Col>
-                </Row>
-            </Container>
-    );
+  return (
+    <Container fluid className="d-flex align-items-center justify-content-center">
+      <Col>
+        <Row className="align-items-center justify-content-center">
+          <QRCode value="https://github.com/gcoro/react-qrcode-logo" size={250} />
+        </Row>
+        <Row>
+          <Form>
+            <div className='mb-3'>
+              <Form.Label htmlFor="amount">Send</Form.Label>
+              <InputGroup>
+                <InputGroup.Text id="amount">EUR</InputGroup.Text>
+                <Form.Control
+                  placeholder="3.14"
+                  aria-label="EUR amount"
+                  aria-describedby="amount-balance"
+                />
+              </InputGroup>
+              <Form.Text id="amount-balance" muted>
+                Current balance is EUR 100,000.00
+              </Form.Text>
+            </div>
+            <div className='mb-3'>
+              <Form.Label htmlFor='send-to'>To</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  id="send-to"
+                  placeholder=""
+                  aria-label="Receiver of the amount"
+                />
+                <SendToScannerModal onScan={(out)=> console.log(out)} onError={(out)=> console.log(out)} />
+              </InputGroup>
+            </div>
+            <Button>Send</Button>
+          </Form >
+        </Row>
+      </Col>
+    </Container>
+  );
+}
 
+type SendToScannerProps = {onScan: (out: QrContent) => void, onError: (out: string) => void};
 
+type QrContent = {
+  publicKey: string
+};
+
+function SendToScannerModal(props: SendToScannerProps) {
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState<string>();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const onDecode = (qrString: string) => {
+    const content = JSON.parse(qrString) as unknown;
+
+    if (!content || typeof content !== "object") {
+      setError(`Expected JSON object, instead got: ${qrString}`);
+      return;
+    }
+    if (!("publicKey" in content)) {
+      setError("Expected JSON object with field 'publicKey'");
+      return;
+    }
+    if (typeof content.publicKey !== 'string') {
+      setError("Unexpected");
+      return;
+    }
+    handleClose();
+    props.onScan(content as QrContent);
+  };
+
+  const onError = (err: Error) => {
+    handleClose();
+    props.onError(err.message);
+  };
+
+  return (
+    <>
+      <Button id="button-send-to-scan" onClick={handleShow}>
+        <FontAwesomeIcon icon={faQrcode}/>
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        centered
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Scan QR for the receiver</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <QrScanner onDecode={onDecode} onError={onError}/>
+        </Modal.Body>
+        <Modal.Footer>
+          {error}
+        </Modal.Footer>
+      </Modal>
+    </>);
 }
