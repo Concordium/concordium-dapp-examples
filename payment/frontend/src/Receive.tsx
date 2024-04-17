2e1cf5948449c071c5adb7727303840cb02f2d47
tschudid <14889202+tschudid@users.noreply.github.com>
2024-04-17 18:02:23 +0200

    changing QR codes

--- payment/frontend/src/Receive.tsx
+++ payment/frontend/src/Receive.tsx
@@ -11,20 +11,50 @@ import { QRCode } from 'react-qrcode-logo';
 import { QrScanner } from '@yudiel/react-qr-scanner';
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
+import * as Keys from "./keys";
+import {Buffer} from "buffer";
 
 import './styles.scss';
 
 export function ReceivePage() {
   const nav = useNavigate();
+  const [amount, setAmount] = useState<bigint>();
   return (
     <Container fluid className="d-flex flex-column align-items-center justify-content-center">
-          <figure className='mt-4'>
-              <h1 className="display-1"><small className="text-muted">EUR</small> 23000.00</h1>
-              <figcaption className="blockquote-footer">Balance</figcaption>
-          </figure>
+          <QRPublic amount={amount}/>
+          <div className='mb-3'>
+            <Form.Label htmlFor="amount">Amount</Form.Label>
+            <InputGroup>
+              <InputGroup.Text id="amount">EUR</InputGroup.Text>
+              <Form.Control
+                name="amount"
+                type='number'
+                placeholder="3.14"
+                aria-label="EUR amount"
+                aria-describedby="amount-balance"
+                onChange={(e) => setAmount(BigInt(Number(e.target.value)*1000000))}
+              />
+            </InputGroup>
+          </div>   
           <div className="d-grid gap-2 w-100 mt-4">
               <Button variant="dark" size="lg" onClick={()=> nav("/")}>Back</Button>
           </div>
     </Container>
   );
+}
+
+type QrProps = {
+  amount?: bigint
+};
+
+function QRPublic(props: QrProps) {
+  const publicKey = Keys.usePublicKey();
+  if (!publicKey) {
+    return "Loading";
+  }
+  const qrContentString = JSON.stringify({
+    publicKey: Buffer.from(publicKey).toString("base64"),
+    request: props.amount?.toString()
+  });
+  return (<QRCode value={qrContentString} size={250} />);
 }
