8877fefdd33d8cf54d4228f1cc1481fe8d0bdb98
Doris Benda <DOBEN@users.noreply.github.com>
2024-07-18 14:36:12 +0200

    Initial indexer

---
+++ compliant-reward-distribution/indexer/src/bin/indexer.rs
@@ -0,0 +1,332 @@
+//! A tool for indexing newly created accounts on Concordium into a
+//! postgres database. The database is configured with the tables from the file
+//! `../resources/schema.sql`. A table
+//! `settings` exists to store global configurations.
+use ::indexer::db::{DatabaseError, DatabasePool};
+use anyhow::Context;
+use clap::Parser;
+use concordium_rust_sdk::{
+    indexer::{self, ContractUpdateInfo, ProcessorConfig, TransactionIndexer},
+    smart_contracts::common::to_bytes,
+    types::{
+        queries::BlockInfo, smart_contracts::OwnedReceiveName, AbsoluteBlockHeight,
+        BlockItemSummary, ContractAddress,
+    },
+    v2::{self as sdk, Client},
+};
+use tokio_postgres::types::{Json, ToSql};
+
+/// Command line configuration of the application.
+#[derive(Debug, clap::Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(
+        long = "node",
+        short = 'n',
+        help = "The node endpoint.",
+        default_value = "https://grpc.testnet.concordium.com:20000",
+        global = true,
+        env = "CCD_INDEXER_NODE"
+    )]
+    node_endpoint: concordium_rust_sdk::v2::Endpoint,
+    /// Database connection string.
+    #[arg(
+        long = "db-connection",
+        default_value = "host=localhost dbname=indexer user=postgres password=password port=5432",
+        help = "A connection string detailing the connection to the database used by the \
+                application.",
+        env = "CCD_INDEXER_DB_CONNECTION"
+    )]
+    db_connection: tokio_postgres::config::Config,
+    /// Maximum log level
+    #[clap(
+        long = "log-level",
+        default_value = "info",
+        help = "The maximum log level. Possible values are: `trace`, `debug`, `info`, `warn`, and \
+                `error`.",
+        env = "CCD_INDEXER_LOG_LEVEL"
+    )]
+    log_level:     tracing_subscriber::filter::LevelFilter,
+}
+
+/// A handler for storing monitored events in the database. This implements
+/// the `indexer::ProcessEvent` trait to store events in the database.
+struct StoreEvents {
+    /// A database pool used for reconnects.
+    db_pool: DatabasePool,
+}
+
+#[indexer::async_trait]
+impl indexer::ProcessEvent for StoreEvents {
+    type Data = (BlockInfo, Vec<BlockItemSummary>);
+    type Description = String;
+    type Error = anyhow::Error;
+
+    async fn process(
+        &mut self,
+        (block_info, block_items): &Self::Data,
+    ) -> Result<Self::Description, Self::Error> {
+        let mut conn = self.db_pool.get().await?;
+
+        // It is typically easiest to reason about a database if blocks are inserted
+        // in a single database transaction. So we do that here.
+        let db_transaction = conn
+            .client
+            .transaction()
+            .await
+            .context("Failed to build database transaction")?;
+
+        let params: [&(dyn ToSql + Sync); 1] = [&(block_info.block_height.height as i64)];
+
+        // Update latest_processed_block_height
+        let statement = db_transaction
+            .prepare_cached(
+                "UPDATE settings SET latest_processed_block_height = $1 WHERE id = true",
+            )
+            .await
+            .context("Failed to prepare latest_processed_block_height transaction")?;
+
+        db_transaction
+            .execute(&statement, &params)
+            .await
+            .context("Failed to execute latest_processed_block_height transaction")?;
+
+        // for single_contract_update_info in contract_update_info {
+        //     for (_contract_invoked, _entry_point_name, events) in
+        //         single_contract_update_info.0.execution_tree.events()
+        //     {
+        //         for (event_index, event) in events.iter().enumerate() {
+        //             // let parsed_event: contract::Event<AdditionalData> =
+        // event.parse()?;
+
+        //             // if let contract::Event::<AdditionalData>::ItemStatusChanged(
+        //             //     item_status_change_event,
+        //             // ) = parsed_event
+        //             // {
+        //             //     let params: [&(dyn ToSql + Sync); 6] = [
+        //             //         &(block_info.block_slot_time),
+        //             //
+        // &single_contract_update_info.0.transaction_hash.as_ref(),
+        // //         &(event_index as i64),             //
+        // &(item_status_change_event.item_id.0 as i64),             //
+        // &Json(&item_status_change_event.new_status),             //
+        // &item_status_change_event.additional_data.bytes,             //
+        // ];
+
+        //             //     let statement = db_transaction
+        //             //         .prepare_cached(
+        //             //             "INSERT INTO item_status_changed_events (id,
+        // block_time, \             //              transaction_hash,
+        // event_index, item_id, new_status, \             //
+        // additional_data) SELECT COALESCE(MAX(id) + 1, 0), $1, $2, $3, \
+        //             //              $4, $5, $6 FROM item_status_changed_events;",
+        //             //         )
+        //             //         .await
+        //             //         .context("Failed to prepare item_status_change_event
+        // transaction")?;
+
+        //             //     db_transaction
+        //             //         .execute(&statement, &params)
+        //             //         .await
+        //             //         .context("Failed to execute item_status_change_event
+        // transaction")?;
+
+        //             //     tracing::debug!(
+        //             //         "Preparing item_status_change_event from block {},
+        // transaction hash \             //          {}, and event index {}.",
+        //             //         block_info.block_height,
+        //             //         single_contract_update_info.0.transaction_hash,
+        //             //         event_index
+        //             //     );
+        //             // } else if let contract::Event::<AdditionalData>::ItemCreated(
+        //             //     item_created_event,
+        //             // ) = parsed_event
+        //             // {
+        //             //     let params: [&(dyn ToSql + Sync); 6] = [
+        //             //         &(block_info.block_slot_time),
+        //             //
+        // &single_contract_update_info.0.transaction_hash.as_ref(),
+        // //         &(event_index as i64),             //
+        // &(item_created_event.item_id.0 as i64),             //
+        // &to_bytes(&item_created_event.metadata_url),             //
+        // &Json(&item_created_event.initial_status),             //     ];
+
+        //             //     let statement = db_transaction
+        //             //         .prepare_cached(
+        //             //             "INSERT INTO item_created_events (id, block_time,
+        // \             //              transaction_hash, event_index, item_id,
+        // metadata_url, \             //              initial_status) SELECT
+        // COALESCE(MAX(id) + 1, 0), $1, $2, $3, $4, \             //
+        // $5, $6 FROM item_created_events;",             //         )
+        //             //         .await
+        //             //         .context("Failed to prepare item_created_event
+        // transaction")?;
+
+        //             //     db_transaction
+        //             //         .execute(&statement, &params)
+        //             //         .await
+        //             //         .context("Failed to execute item_created_event
+        // transaction")?;
+
+        //             //     tracing::debug!(
+        //             //         "Preparing event from block {}, transaction hash {},
+        // and event index \             //          {}.",
+        //             //         block_info.block_height,
+        //             //         single_contract_update_info.0.transaction_hash,
+        //             //         event_index
+        //             //     );
+        //             // }
+        //         }
+        //     }
+        // }
+        // Commit the transaction
+        db_transaction
+            .commit()
+            .await
+            .context("Failed to commit block transaction")?;
+
+        // We return an informative message that will be logged by the `process_events`
+        // method of the indexer.
+        Ok(format!(
+            "Processed block {} at height {} with timestamp {}.",
+            block_info.block_hash, block_info.block_height, block_info.block_slot_time
+        ))
+    }
+
+    async fn on_failure(
+        &mut self,
+        error: Self::Error,
+        _failed_attempts: u32,
+    ) -> Result<bool, Self::Error> {
+        tracing::error!("Encountered error {error}");
+
+        Ok(true)
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app: Args = Args::parse();
+
+    // Tracing configuration.
+    {
+        use tracing_subscriber::prelude::*;
+        let log_filter = tracing_subscriber::filter::Targets::new()
+            .with_target(module_path!(), app.log_level)
+            .with_target("ccd_indexer", app.log_level)
+            .with_target("ccd_event_processor", app.log_level)
+            .with_target("tokio_postgres", app.log_level);
+
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(log_filter)
+            .init();
+    }
+
+    // Set up endpoint to the node.
+    let endpoint = if app
+        .node_endpoint
+        .uri()
+        .scheme()
+        .map_or(false, |x| x == &sdk::Scheme::HTTPS)
+    {
+        app.node_endpoint
+            .tls_config(tonic::transport::channel::ClientTlsConfig::new())
+            .context("Unable to construct TLS configuration for the Concordium API.")?
+    } else {
+        app.node_endpoint
+    }
+    .connect_timeout(std::time::Duration::from_secs(5))
+    .timeout(std::time::Duration::from_secs(10));
+
+    // Establish connection to the blockchain node.
+    let mut client = Client::new(endpoint.clone()).await?;
+    let consensus_info = client.get_consensus_info().await?;
+
+    // Establish connection to the postgres database.
+    let db_pool = DatabasePool::create(app.db_connection.clone(), 2, true)
+        .await
+        .context("Could not create database pool")?;
+    let db = db_pool
+        .get()
+        .await
+        .context("Could not get database connection from pool")?;
+
+    let settings = db
+        .get_settings()
+        .await
+        .context("Could not get settings from database")?;
+
+    let start_block = match settings {
+        // If the indexer is re-started with the same database settings,
+        // it should resume indexing from the `latest_processed_block_height+1` as stored in the
+        // database.
+        Some(settings) => {
+            // This check ensures when re-starting the indexer, that the current
+            // `genesis_hash/node` settings of the indexer are compatible with the
+            // stored indexer settings to prevent corrupting the database.
+            anyhow::ensure!(
+                settings.genesis_block_hash == consensus_info.genesis_block,
+                "Genesis hash from the connected node {} does not match the genesis hash {} found \
+                 in the database",
+                consensus_info.genesis_block,
+                settings.genesis_block_hash
+            );
+
+            // While it is possible to start the indexer and immediatly stop the indexer,
+            // so that the settings are set in the database but no block has been processed
+            // at all, this edge case is rather theoretically and should not
+            // happen in practice (the default value here should not happen in practice).
+            let last_processed_block = settings
+                .latest_processed_block_height
+                .unwrap_or(consensus_info.last_finalized_block_height);
+
+            last_processed_block.next()
+        }
+        // If the indexer is started for the first time, lookup the last block finalized, initialize
+        // the database, and start to index at the last finalized block.
+        None => {
+            let start_block = consensus_info.last_finalized_block_height;
+
+            db.init_settings(&consensus_info.genesis_block, start_block)
+                .await
+                .context("Could not init settings for database")?;
+
+            start_block
+        }
+    };
+
+    tracing::info!(
+        "Indexing on network with genesis hash {}.",
+        consensus_info.genesis_block
+    );
+
+    handle_indexing(endpoint, start_block, db_pool).await
+}
+
+/// Handle indexing events.
+async fn handle_indexing(
+    endpoint: sdk::Endpoint,
+    start_block: AbsoluteBlockHeight,
+    db_pool: DatabasePool,
+) -> anyhow::Result<()> {
+    tracing::info!("Indexing from block height {}.", start_block);
+
+    let traverse_config = indexer::TraverseConfig::new_single(endpoint, start_block);
+
+    let events = StoreEvents { db_pool };
+
+    // The program terminates only
+    // when the processor terminates, which in this example can only happen if
+    // there are sufficiently many errors when attempting to write to the
+    // database.
+    indexer::traverse_and_process(
+        traverse_config,
+        TransactionIndexer {},
+        ProcessorConfig::new(),
+        events,
+    )
+    .await?;
+
+    Ok(())
+}
