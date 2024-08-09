795c34ca442584b75a53318cde03a20d94f9e086
benya7 <bitup.games@gmail.com>
2024-08-09 18:26:43 +0200

    fix: read usageLimit from env vars

--- testnet-faucet/src/pages/api/usageLimit.ts
+++ testnet-faucet/src/pages/api/usageLimit.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import checkUsageLimit from '@/lib/checkUsageLimit';
 
 interface IBody {
-    hoursLimit: number;
     receiver: string;
 }
 
@@ -16,15 +15,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed. Please use POST.' });
     }
-    const { hoursLimit, receiver } = req.body as IBody;
+    const { receiver } = req.body as IBody;
 
-    if (!hoursLimit || !receiver) {
+    if (!receiver) {
         return res.status(400).json({
-            error: 'Missing parameters. Please provide hoursLimit and receiver params.',
+            error: 'Missing parameters. Please provide and receiver params.',
         });
     }
     try {
-        const isAllowed = await checkUsageLimit(hoursLimit, receiver);
+        const isAllowed = await checkUsageLimit(receiver);
         return res.status(200).json({ isAllowed });
     } catch (e) {
         return res.status(500).json({ error: `An unexpected error has occurred: ${e}` });
