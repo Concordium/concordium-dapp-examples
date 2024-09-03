777660019b9e4331c378ec92307264f16ce730e8
Doris Benda <DOBEN@users.noreply.github.com>
2024-09-03 15:08:03 +0200

    Add ZK proof generation

--- compliant-reward-distribution/frontend/src/components/ZkProofSubmission.tsx
+++ compliant-reward-distribution/frontend/src/components/ZkProofSubmission.tsx
@@ -1,40 +1,90 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, Button, Form } from 'react-bootstrap';
-import { useForm, useWatch } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import { WalletProvider } from '../../wallet-connection';
+import { ConcordiumGRPCClient, CredentialStatement } from '@concordium/web-sdk';
+import { getStatement, submitZkProof } from '../utils';
+import { CONTEXT_STRING } from '../constants';
+import sha256 from 'sha256';
 
 interface Props {
     accountAddress: string | undefined;
     provider: WalletProvider | undefined;
+    grpcClient: ConcordiumGRPCClient | undefined;
 }
 
 export function ZkProofSubmission(props: Props) {
-    const { provider } = props;
-
-    interface FormType {
-        zkStatement: JSON | undefined;
-    }
-    const { control, handleSubmit } = useForm<FormType>({ mode: 'all' });
-
-    const [zkStatement] = useWatch({
-        control: control,
-        name: ['zkStatement'],
-    });
+    const { provider, grpcClient } = props;
 
     const [error, setError] = useState<string | undefined>(undefined);
+    const [zkStatement, setZkStatement] = useState<CredentialStatement | undefined>(undefined);
+
+    useEffect(() => {
+        const fetchStatement = async () => {
+            const statement: CredentialStatement = await getStatement();
+            setZkStatement(statement);
+        };
+
+        fetchStatement();
+    }, []);
+
+    interface FormType { }
+    const { handleSubmit } = useForm<FormType>({ mode: 'all' });
 
     async function onSubmit() {
         setError(undefined);
 
+        if (grpcClient === undefined) {
+            setError(`'grpcClient' is undefined`);
+            throw Error(`'grpcClient' is undefined`);
+        }
+
+        const bestBlockHeight = (await grpcClient.client.getConsensusInfo(''))?.response.bestBlockHeight;
+
+        if (bestBlockHeight === undefined) {
+            setError(`Couldn't get 'bestBlockHeight' from chain`);
+            throw Error(`Couldn't get 'bestBlockHeight' from chain`);
+        }
+
+        const recentBlockHeight = bestBlockHeight.value - 10n;
+
+        const recentBlockHash = (
+            await grpcClient.client.getBlocksAtHeight({
+                // TODO: Type in web-sdk needs to be fixed to do this ergonomically.
+                blocksAtHeight: {
+                    oneofKind: 'absolute',
+                    absolute: {
+                        height: { value: recentBlockHeight },
+                    },
+                },
+            })
+        )?.response.blocks[0].value;
+
+        if (recentBlockHash === undefined) {
+            setError(`'recentBlockHash' is undefined`);
+            throw Error(`'recentBlockHash' is undefined`);
+        }
+
+        const hashDigest = [recentBlockHash, CONTEXT_STRING];
+        const challenge = sha256(hashDigest.flatMap((item) => Array.from(item)));
+
         if (zkStatement === undefined) {
-            setError(`'statement' input field is undefined`);
-            throw Error(`'statement' input field is undefined`);
+            setError(`'zkStatement' is undefined`);
+            throw Error(`'zkStatement' is undefined`);
         }
 
         if (provider === undefined) {
             setError(`'provider' is undefined`);
             throw Error(`'provider' is undefined`);
+        }
+
+        const presentation = await provider.requestVerifiablePresentation(challenge, [zkStatement]);
+
+        try {
+            await submitZkProof(presentation, recentBlockHeight);
+        } catch (error) {
+            setError((error as Error).message);
         }
     }
 
@@ -44,12 +94,6 @@ export function ZkProofSubmission(props: Props) {
                 <h2 className="centered"> Submit ZK Proof</h2>
                 <br />
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    {/* <Form.Group className="col mb-3">
-                        <Form.Label>ZK statement</Form.Label>
-                        <Form.Control {...register('zkStatement', { required: true })} placeholder="Enter metadata URL" />
-                        {formState.errors.zkStatement && <Alert variant="info"> ZK statement is required </Alert>}
-                        <Form.Text />
-                    </Form.Group> */}
                     <Button variant="secondary" type="submit">
                         Submit
                     </Button>
