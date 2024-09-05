c5f608d54b720d398d7ea49b9531899984784b7b
Doris Benda <DOBEN@users.noreply.github.com>
2024-09-05 14:21:39 +0200

    Add set claimed flow

--- compliant-reward-distribution/frontend/src/components/AdminSetClaimed.tsx
+++ compliant-reward-distribution/frontend/src/components/AdminSetClaimed.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import { Alert, Button, Form } from 'react-bootstrap';
 
-import { validateAccountAddress } from '../utils';
+import { getARecentBlockHash, setClaimed, validateAccountAddress } from '../utils';
 import { ConcordiumGRPCClient } from '@concordium/web-sdk';
 
 interface Props {
@@ -25,17 +25,32 @@ export function AdminSetClaimed(props: Props) {
 
     const [error, setError] = useState<string | undefined>(undefined);
 
-    function onSubmit() {
+    async function onSubmit() {
         setError(undefined);
 
-        if (!address) {
-            setError(`'address' input field is undefined`);
-            throw Error(`'address' input field is undefined`);
-        }
+        try {
+            if (!address) {
+                setError(`'address' input field is undefined`);
+                throw Error(`'address' input field is undefined`);
+            }
 
-        if (!signer) {
-            setError(`'signer' is undefined. Connect your wallet.`);
-            throw Error(`'signer' is undefined. Connect your wallet.`);
+            if (!signer) {
+                setError(`'signer' is undefined. Connect your wallet.`);
+                throw Error(`'signer' is undefined. Connect your wallet.`);
+            }
+
+            const [recentBlockHash, recentBlockHeight] = await getARecentBlockHash(grpcClient);
+            console.log(recentBlockHash);
+            // TODO: add signature generation
+
+            await setClaimed(
+                signer,
+                'c4bb83e7d7a9e6fe7a1b5f527174f7e368db9385b25fce0f4e4b7190781e57b5de6ad65a7481f1038f859d4c4ba8c07ed649b84f9e3c17e2bbdb87cf527cd602',
+                recentBlockHeight,
+                address,
+            );
+        } catch (error) {
+            setError((error as Error).message);
         }
     }
 
