92d09d29d5d1969e40254d008876d36870255b78
fistasolutions <fistasolutions@gmail.com>
2024-10-30 16:57:39 +0500

    account button

--- compliant-reward-distribution/frontend/src/components/Admin/Admin.tsx
+++ compliant-reward-distribution/frontend/src/components/Admin/Admin.tsx
@@ -22,31 +22,8 @@ export function Admin(props: Props) {
     const capitalizedNetwork = CONFIG.network[0].toUpperCase() + CONFIG.network.substring(1);
     return (
         <div className="centered bg-dark min-vh-100">
-            {connectedAccount ?
-                <>
-                    <div className="d-flex justify-content-center mb-3">
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href={`https://github.com/Concordium/concordium-dapp-examples/tree/main/compliant-reward-distribution`}
-                        >
-                            Version {version} ({capitalizedNetwork})
-                        </a>
-                    </div>
-                    <div className="d-flex justify-content-center mb-3">
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href={`#`}
-                        >
-                            {connectedAccount
-                                ? connectedAccount.slice(0, 5) + '...' + connectedAccount.slice(-5)
-                                : 'No Account Connected'}
-                        </a>
-                    </div>
-                </>
-
-                : <ConnectWalletAdmin />}
+            
+            <ConnectWalletAdmin />
             <AdminGetPendingApprovals provider={provider} signer={connectedAccount} grpcClient={grpcClient} />
             <AdminGetAccountData provider={provider} signer={connectedAccount} grpcClient={grpcClient} />
             <AdminSetClaimed provider={provider} signer={connectedAccount} grpcClient={grpcClient} />
