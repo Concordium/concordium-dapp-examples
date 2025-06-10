0d046b43b5abfb59eea07ab643f0b9b58121a4ce
Dragos-Gabriel Danciulescu <dragos-gabrieldanciulescu@MacBook-Pro-tilhrende-Dragos-Gabriel.local>
2025-06-10 11:34:12 +0300

    Test pipeline

--- allowListManagerFaucet/frontend/build-and-serve.sh
+++ allowListManagerFaucet/frontend/build-and-serve.sh
@@ -8,7 +8,7 @@ echo "BACKEND_URL: ${BACKEND_URL}"
 # Build the application with runtime environment variables
 npm run build
 
-echo "Build completed. Starting server..."
+echo "Build completed.Starting server..."
 
 # Serve the built application
 serve -s dist -l 3000
