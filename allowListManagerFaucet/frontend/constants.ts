62b156fb50f084b09d0ab7dd044aacadb953a15f
dragosgd <dragosdanciulescug@gmail.com>
2025-10-31 14:54:43 +0200

    Remove relayUrl

--- allowListManagerFaucet/frontend/constants.ts
+++ allowListManagerFaucet/frontend/constants.ts
@@ -1,19 +1,16 @@
 import { SignClientTypes } from "@walletconnect/types";
 import { BrowserWalletConnector, WalletConnectConnector } from '@concordium/wallet-connectors';
-import { ephemeralConnectorType } from '@concordium/react-components';
+import { ephemeralConnectorType, CONCORDIUM_WALLET_CONNECT_PROJECT_ID } from '@concordium/react-components';
 
 export const REGISTRY_CONTRACT_REGISTRY_METADATA_RETURN_VALUE_SCHEMA =
     'FAADAAAADwAAAGlzc3Vlcl9tZXRhZGF0YRQAAgAAAAMAAAB1cmwWAQQAAABoYXNoFQIAAAAEAAAATm9uZQIEAAAAU29tZQEBAAAAHiAAAAAPAAAAY3JlZGVudGlhbF90eXBlFAABAAAADwAAAGNyZWRlbnRpYWxfdHlwZRYAEQAAAGNyZWRlbnRpYWxfc2NoZW1hFAABAAAACgAAAHNjaGVtYV9yZWYUAAIAAAADAAAAdXJsFgEEAAAAaGFzaBUCAAAABAAAAE5vbmUCBAAAAFNvbWUBAQAAAB4gAAAA';
 
-const WALLET_CONNECT_PROJECT_ID = '76324905a70fe5c388bab46d3e0564dc';
-
 const WALLET_CONNECT_OPTS: SignClientTypes.Options = {
-    projectId: WALLET_CONNECT_PROJECT_ID,
-    relayUrl: 'wss://relay.walletconnect.com',
+    projectId: CONCORDIUM_WALLET_CONNECT_PROJECT_ID,
     metadata: {
         name: 'Token Distribution dApp',
         description: 'Compliance token distribution using Concordium ID',
-        url: typeof window !== 'undefined' ? window.location.origin : 'https://localhost:5173',
+        url: '#',
         icons: ['https://walletconnect.com/walletconnect-logo.png'],
     },
 };
