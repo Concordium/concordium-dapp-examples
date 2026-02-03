c3c286c4a5f87b480cf35f2c6f4812cf8dbfd43f
rba <rba@concordium.com>
2026-02-03 15:09:02 +0000

    fixing esbuild.config.ts

--- nft-minting/esbuild.config.ts
+++ nft-minting/esbuild.config.ts
@@ -17,8 +17,8 @@ const config: BuildOptions = {
     minify: true,
     metafile: true,
     logLevel: 'info',
-    sourcemap: watch ? 'inline' : 'external',
-    target: ['es2020'],
+    sourcemap: 'inline',
+    target: ['chrome67'],
     outdir: 'dist',
     plugins: [
         htmlPlugin({
@@ -36,11 +36,19 @@ const config: BuildOptions = {
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-    if (watch) {
-        const ctx = await esbuild.context(config);
-        await ctx.watch();
-        console.log('watching for changes...');
-    } else {
-        esbuild.build(config).catch(() => process.exit(1));
+    try {
+        if (watch) {
+            const ctx = await esbuild.context(config);
+            await ctx.watch();
+            console.log('watching for changes...');
+        } else {
+                // AWAIT THE BUILD
+                await esbuild.build(config);
+                console.log('Build completed successfully.');
+                // Then the script ends naturally here with exit code 0
+        } 
+    } catch (error) {
+        console.error('Build failed:', error);
+        process.exit(1);
     }
 })();
