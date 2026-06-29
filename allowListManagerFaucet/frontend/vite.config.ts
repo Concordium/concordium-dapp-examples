5fcc1a80f90c2ab6529ea832e1247a046e653a62
Andy Nelson <an@concordium.com>
2026-06-29 16:01:01 +0100

    Update allowListManagerFaucet/frontend/vite.config.ts
    remove unneeded import
    
    Co-authored-by: Søren Bruus Zeppelin <soerenbf@gmail.com>

--- allowListManagerFaucet/frontend/vite.config.ts
+++ allowListManagerFaucet/frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
